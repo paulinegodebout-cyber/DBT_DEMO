05d07af0ec5d4d921f9e5b70530c112ba0268e3c
Pauline Godebout <pauline.godebout@gmail.com>
2026-02-26 12:32:07 +0100

    Add mandatory tests

--- connexion/models/staging/stg_orders.sql
+++ connexion/models/staging/stg_orders.sql
@@ -1,16 +1,15 @@
 with source as (
     select * from {{ source('my_dbt_db', 'raw_orders') }}
 ),
-renamed as (
+ renamed as (
     select
-        id as orders_id,
-        customer as orders_customer,
-        ordered_at as orders_ordered_at,
-        store_id as orders_store_id,
-        subtotal as orders_subtotal,
-        tax_paid as orders_tax_paid,
-        order_total as orders_order_total
-        
+        id as order_id,           
+        customer as customer_id,  
+        ordered_at as order_date, 
+        store_id,                 
+        subtotal,
+        tax_paid,
+        order_total
     from source
 )
 select * from renamed
